a243219beddefdf8f7e8975d1ab8a125b8260f29
Jayden Gillam <jaydster01@gmail.com>
2023-10-18 17:53:09 -0400

    made test for returning a json dictionary and began writing code for parsing through dictionary

--- project1.py
+++ project1.py
@@ -1,11 +1,35 @@
-def main():
-    condense_page_name("   Ball State University    ")
-    
+import json
+import ssl
+from urllib.request import urlopen
+import pprint
 
-def condense_page_name(page_name: str):
+def main():
+    x = condense_page_name("Ball State")
+    y = get_json_dict(x)
+    display_information(y)
+def condense_page_name(page_name: str) -> str:
     condensed_page_name = page_name.strip()
     condensed_page_name = condensed_page_name.replace(" ", "_")
     return condensed_page_name
+
+def get_json_dict(condensed_page_name: str) -> dict:
+    url = f"https://en.wikipedia.org/w/api.php?action=query&prop=revisions&meta=siteinfo&titles={condensed_page_name}&rvprop=user|timestamp&continue=&redirects&rvlimit=30&format=json"
+    context = ssl._create_unverified_context()
+    response = urlopen(url, context=context)
+    return json.loads(response.read())
+
+def display_information(json_dict: dict) -> None:
+    try:
+        for item in json_dict["query"]["redirects"]:
+            print(f"Redirected from {item['from']} to {item['to']}\n")
+    except:
+        pass
+    for item in json_dict["query"]["pages"].values():
+        for revision in item["revisions"]:
+            print(f"{revision["timestamp"]} {revision["user"]}\n")
+
+
+
 
 
 if __name__ == "__main__":
