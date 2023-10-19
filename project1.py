19d7f55740c2fcc157c7883c52de018634651fcd
Jayden Gillam <jaydster01@gmail.com>
2023-10-19 14:09:03 -0400

    Added user input and renamed variables

--- project1.py
+++ project1.py
@@ -1,22 +1,25 @@
 import json
 import ssl
 from urllib.request import urlopen
-import pprint
 
 def main():
-    x = condense_page_name("Ball State")
-    y = get_json_dict(x)
-    display_information(y)
+    article_name = condense_page_name(input("Enter name of Wikipedia article: "))
+    json_dict = get_json_dict(article_name)
+    display_information(json_dict)
+
+
 def condense_page_name(page_name: str) -> str:
     condensed_page_name = page_name.strip()
     condensed_page_name = condensed_page_name.replace(" ", "_")
     return condensed_page_name
+
 
 def get_json_dict(condensed_page_name: str) -> dict:
     url = f"https://en.wikipedia.org/w/api.php?action=query&prop=revisions&meta=siteinfo&titles={condensed_page_name}&rvprop=user|timestamp&continue=&redirects&rvlimit=30&format=json"
     context = ssl._create_unverified_context()
     response = urlopen(url, context=context)
     return json.loads(response.read())
+
 
 def display_information(json_dict: dict) -> None:
     try:
@@ -27,9 +30,6 @@ def display_information(json_dict: dict) -> None:
     for item in json_dict["query"]["pages"].values():
         for revision in item["revisions"]:
             print(f"{revision["timestamp"]} {revision["user"]}\n")
-
-
-
 
 
 if __name__ == "__main__":
