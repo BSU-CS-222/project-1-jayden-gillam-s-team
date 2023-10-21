125f9659a89aaacb32b2a2d4461135d86c1ec548
Jayden Gillam <jaydster01@gmail.com>
2023-10-21 03:26:02 -0400

    Added error checking for network error, no article found error and if no article name is provided.

--- project1.py
+++ project1.py
@@ -1,9 +1,13 @@
 import json
 import ssl
 from urllib.request import urlopen
+import sys
 
 def main():
     article_name = condense_page_name(input("Enter name of Wikipedia article: "))
+    if article_name == "":
+        print("Please provide an article name.")
+        return sys.exit(1)
     json_dict = get_json_dict(article_name)
     display_information(json_dict)
 
@@ -17,7 +21,11 @@ def condense_page_name(page_name: str) -> str:
 def get_json_dict(condensed_page_name: str) -> dict:
     url = f"https://en.wikipedia.org/w/api.php?action=query&prop=revisions&meta=siteinfo&titles={condensed_page_name}&rvprop=user|timestamp&continue=&redirects&rvlimit=30&format=json"
     context = ssl._create_unverified_context()
-    response = urlopen(url, context=context)
+    try:
+        response = urlopen(url, context=context)
+    except:
+        print("Sorry there is a network error.")
+        return sys.exit(3)
     return json.loads(response.read())
 
 
@@ -27,10 +35,13 @@ def display_information(json_dict: dict) -> None:
             print(f"Redirected from {item['from']} to {item['to']}\n")
     except:
         pass
-    for item in json_dict["query"]["pages"].values():
-        for revision in item["revisions"]:
-            print(f"{revision["timestamp"]} {revision["user"]}\n")
-
+    try:
+        for item in json_dict["query"]["pages"].values():
+            for revision in item["revisions"]:
+                print(f"{revision['timestamp']} {revision['user']}\n")
+    except:
+        print("No article found.")
+        return sys.exit(2)
 
 if __name__ == "__main__":
     main()
